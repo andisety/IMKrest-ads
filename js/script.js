b62c2b7f7d7d2203906d9777a87914676c07d967
andisety <purnomoandi126@gmail.com>
2022-11-20 10:43:30 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Animations init
+
+
+// document.querySelector("#btnOrder ").addEventListener("click", function(){
+//      alert("Hello World!");
+//      });
+
+// document.querySelector(".col-3 ").addEventListener("click", function(){
+//      alert("Hello World!");
+//      });
+
+const btn = document.querySelectorAll("#btnOrder");
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function(){alert("items have been added")})}
+
+const card = document.querySelectorAll(".card");
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click")}
+  
+
